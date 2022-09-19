2badedb8d94c0a05862651eff2775b4a435353cb
Gusts Linkevics <gusts@gst.lv>
2022-09-19 20:34:55 +0300

    Add project files.

---
+++ Library.Data/Book.cs
@@ -0,0 +1,18 @@
+﻿namespace Library.Data
+{
+    public class Book
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public Author Author { get; set; }
+
+        public GenreEnum Genre { get; set; }
+
+        public string[] Keywords { get; set; }
+
+        public byte Rating { get; set; }
+
+    }
+}
